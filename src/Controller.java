f74bb714746db55c8b072ca9c07f444d4fd4268f
Parth <maheshwariparth234@gmail.com>
2022-11-08 01:49:35 +0530

    Sliders added

--- src/Controller.java
+++ src/Controller.java
@@ -3,9 +3,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -29,12 +33,18 @@ public class Controller implements Initializable {
     private String filePath;
     private FileChooser fileChooser;
 
+    @FXML
+    Slider timeSlider = new Slider();
+
+    @FXML
+    Slider volSlider = new Slider();
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         stage = new Stage();
 
         filePath = "C:\\Users\\mahes\\Downloads\\parody.mp3";
+        // filePath = "C:\\Songs\\videoplayback.mp4";
 
         file = new File(filePath);
         media = new Media(file.toURI().toString());
@@ -45,9 +55,40 @@ public class Controller implements Initializable {
     public void setPlayer() {
         media = new Media(file.toURI().toString());
         mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.seek(mediaPlayer.getStartTime());
+        volSlider.setValue(100);
+        timeSlider.setValue(0);
         mediaPlayer.setOnReady(() -> stage.sizeToScene());
         mediaView.setMediaPlayer(mediaPlayer);
         mediaPlayer.setAutoPlay(true);
+
+        // Time Slider
+        mediaPlayer.currentTimeProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable ob) {
+                updatesValues();
+            }
+        });
+
+        // Jump to particular time
+        timeSlider.valueProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable ob) {
+                if (timeSlider.isPressed()) {
+                    mediaPlayer.seek(mediaPlayer.getMedia().getDuration().multiply(timeSlider.getValue()/100));
+                }
+            }
+        });
+
+        // Volume Slider
+        volSlider.valueProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable arg0) {
+                if (volSlider.isPressed()) {
+                    mediaPlayer.setVolume(volSlider.getValue()/100);
+                }
+            }
+        });
     }
     
     public void playMedia() {
@@ -71,7 +112,20 @@ public class Controller implements Initializable {
             new ExtensionFilter("Audio Files", "*.mp3","*.wav"));
         file = fileChooser.showOpenDialog(stage);
         if (file != null) {
+            mediaPlayer.stop();
             setPlayer();
         }
+    }
+
+    protected void updatesValues()
+    {
+        Platform.runLater(new Runnable() {
+            public void run()
+            {
+                // Updating to the new time value
+                // This will move the slider while running your video
+                timeSlider.setValue(mediaPlayer.getCurrentTime().toMillis() * 100);
+            }
+        });
     }
 }
